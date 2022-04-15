fc3445536b08a42bfc1ebf06f28c9b0755b41be9
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-04-15 14:34:51 +0300

    Fix issue's CodeClimate

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -22,15 +22,18 @@ def generate_diff(f1, f2):
         if n in keys1 and n in keys2:
             resf1 = true_and_false(file1[n])
             resf2 = true_and_false(file2[n])
+            similar_elements = "\t" + str(n) + ": " + resf1 + "\n"
+            first_element_dif = "\t" + "- " + str(n) + ": " + resf1 + "\n"
+            second_element_dif = "\t" + "+ " + str(n) + ": " + resf2 + "\n"
             if file1[n] == file2[n]:
-                result += "\t" + str(n) + ": " + resf1 + "\n"
+                result += similar_elements
             else:
-                result += "\t" + "- " + str(n) + ": " + resf1 + "\n"
-                result += "\t" + "+ " + str(n) + ": " + resf2 + "\n"
+                result += first_element_dif
+                result += second_element_dif
         elif n in keys1 and n not in keys2:
-            result += "\t" + "- " + str(n) + ": " + resf1 + "\n"
+            result += first_element_dif
         else:
-            result += "\t" + "+ " + str(n) + ": " + resf2 + "\n"
+            result += second_element_dif
 
     result += "}"
     print(result)
