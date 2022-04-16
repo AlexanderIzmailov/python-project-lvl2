0d179580fc6673049aa9fca3997a1d78443ac7f6
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-04-16 09:10:07 +0300

    Parser in the differ module

---
+++ gendiff/gendiff_parser.py
@@ -0,0 +1,32 @@
+def true_and_false(element):
+    element = str(element)
+    if element == "True" or element == "False":
+        return element.lower()
+    else:
+        return element
+
+
+def parser(data1, data2):
+    uniq_keys = sorted(set(data1) | set(data2))
+
+    keys1 = list(data1.keys())
+    keys2 = list(data2.keys())
+
+    result = "{\n"
+    for n in uniq_keys:
+        val1_lower = true_and_false(data1.get(n))
+        val2_lower = true_and_false(data2.get(n))
+
+        if n in keys1 and n in keys2:
+            if data1[n] == data2[n]:
+                result += "\t{}: {}\n".format(str(n), val1_lower)
+            else:
+                result += "\t- {}: {}\n".format(str(n), val1_lower)
+                result += "\t+ {}: {}\n".format(str(n), val2_lower)
+        elif n in keys1 and n not in keys2:
+            result += "\t- {}: {}\n".format(str(n), val1_lower)
+        else:
+            result += "\t+ {}: {}\n".format(str(n), val2_lower)
+
+    result += "}"
+    return result
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,12 +1,5 @@
+from .gendiff_parser import parser
 import json
-
-
-def lo(element):
-    element = str(element)
-    if element == "True" or element == "False":
-        return element[0:1].lower() + element[1:]
-    else:
-        return element
 
 
 def generate_diff(f1, f2):
@@ -18,22 +11,11 @@ def generate_diff(f1, f2):
 
     if len(keys1) == 0 and len(keys2) == 0:
         return {}
+    
+    if len(file1) == 0 and len(file2) == 0:
+        return {}
 
-    uniq_keys = sorted(set(keys1) | set(keys2))
+    result = parser(file1, file2)
 
-    result = "{\n"
-    for n in uniq_keys:
-        if n in keys1 and n in keys2:
-            if file1[n] == file2[n]:
-                result += "\t" + str(n) + ": " + lo(file1[n]) + "\n"
-            else:
-                result += "\t" + "- " + str(n) + ": " + lo(file1[n]) + "\n"
-                result += "\t" + "+ " + str(n) + ": " + lo(file2[n]) + "\n"
-        elif n in keys1 and n not in keys2:
-            result += "\t" + "- " + str(n) + ": " + lo(file1[n]) + "\n"
-        else:
-            result += "\t" + "+ " + str(n) + ": " + lo(file2[n]) + "\n"
-
-    result += "}"
     print(result)
     return result
